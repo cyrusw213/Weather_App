5a5e4a693cfdc534d3b82c62edb386ec306d2e3d
Cyrus Wickersham <cyruswickersham@Cyruss-MacBook-Air.local>
2022-05-09 15:45:33 -0400

    forgot to push, have added first two days of forecasts. Trying to fix html/css so that the forecasts do not overlap.

--- js/script.js
+++ js/script.js
@@ -16,15 +16,26 @@
 
 
 // variables
-const URL = "https://api.openweathermap.org/data/2.5/forecast?zip="
+const URL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&zip="
 
 
 // element references
 const $cityOne = $('.cityOne');
 const $inputOne = $('input')
 const $form = $('form');
-const $forecastOne = $('.forecastOne')
-const $forecastListOne = $('<li>')
+const $dateTimeOne = $('.dateTimeOne')
+const $forecastDayOne = $('.forecastDayOne')
+const $tempOneDayOne = $('<li>')
+const $feelsLikeOneDayOne = $('<li>')
+const $weatherOne = $('<li>')
+const $windSpeed = $('<li>')
+const $dateTimeTwo = $('.dateTimeTwo')
+const $forecastDayTwo = $('.forecastDayTwo')
+const $feelsLikeOneDayTwo = $('<li>')
+const $weatherTwo = $('<li>')
+const $windSpeedDayTwo = $('<li>')
+const $tempOneDayTwo = $('<li>')
+
 
 // event listeners
 $form.on('submit', handleGetData)
@@ -40,9 +51,25 @@ function handleGetData(e){
       console.log('working')
       console.log(data)
       $cityOne.text(data.city.name)
-      $forecastOne.text('Date & Time: ' + data.list[0].dt_txt)
-      $forecastListOne.text('Temp: ' + data.list[0].main.temp)
-      $forecastOne.append($forecastListOne)
+      $dateTimeOne.text('Date & Time: ' + data.list[0].dt_txt)
+      $tempOneDayOne.text('Temp: ' + data.list[0].main.temp + ' °F')
+      $feelsLikeOneDayOne.text('Feels Like ' + data.list[0].main.feels_like + ' °F')
+      $weatherOne.text(data.list[0].weather[0].main)
+      $windSpeed.text('Wind Speeds: ' + data.list[0].wind.speed + ' MPH')
+      $forecastDayOne.append($tempOneDayOne)
+      $forecastDayOne.append($feelsLikeOneDayOne)
+      $forecastDayOne.append($weatherOne)
+      $forecastDayOne.append($windSpeed)
+      // second day of first forecasts
+      $dateTimeTwo.text('Date & Time: ' + data.list[8].dt_txt)
+      $feelsLikeOneDayTwo.text('Feels Like ' + data.list[8].main.feels_like + ' °F')
+      $tempOneDayTwo.text('Temp: ' + data.list[8].main.temp + ' °F')
+      $weatherTwo.text(data.list[8].weather[0].main)
+      $windSpeedDayTwo.text('Wind Speeds: ' + data.list[8].wind.speed + ' MPH')
+      $forecastDayTwo.append($tempOneDayTwo)
+      $forecastDayTwo.append($feelsLikeOneDayTwo)
+      $forecastDayTwo.append($weatherTwo)
+      $forecastDayTwo.append($windSpeedDayTwo)
     },
    function(error) {
     console.log('something is wrong')
